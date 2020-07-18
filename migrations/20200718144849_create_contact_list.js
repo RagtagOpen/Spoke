d7adc351a17be3005843f20e69b552df32f53289
Emily Giurleo <e.m.giurleo@gmail.com>
2020-07-18 14:56:24 -0400

    add migration to create contact list

---
+++ migrations/20200718144849_create_contact_list.js
@@ -0,0 +1,27 @@
+exports.up = async knex => {
+  await knex.schema.createTableIfNotExists("contact_list", table => {
+    table.increments("id");
+    table
+      .integer("campaign_id")
+      .notNullable()
+      .references("id")
+      .inTable("campaign")
+      .index();
+  });
+
+  await knex.schema.alterTable("campaign_contact", table => {
+    table
+      .integer("contact_list_id")
+      .nullable()
+      .references("id")
+      .inTable("contact_list")
+      .index();
+  });
+};
+
+exports.down = async knex => {
+  await knex.schema.dropTable("contact_list");
+  await knex.schema.alterTable("campaign_contact", table => {
+    table.dropColumn("contact_list_id");
+  });
+};
